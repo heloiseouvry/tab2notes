6432c2588da9367b28270077489f52160afb5cfd
Heloise Ouvry <heloiseouvry@gmail.com>
2022-01-08 20:33:49 +0100

    Initialisation of parts dict

--- src/f1.py
+++ src/f1.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import preprocess
 
 parser = argparse.ArgumentParser()
@@ -8,6 +9,19 @@ parser.add_argument('-v','--verbose', help='more verbose')
 args = parser.parse_args()
 
 if __name__ == "__main__":
+
+    nb_parts = 4
+    empty_array = [[] for i in range(nb_parts)]
+    parts = {
+        'original': copy.deepcopy(empty_array),
+        'inv': copy.deepcopy(empty_array),
+        'thresh': copy.deepcopy(empty_array),
+        'wo_staff': copy.deepcopy(empty_array),
+        'translated': copy.deepcopy(empty_array),
+        'staff_line' : copy.deepcopy(empty_array),
+        'staff_col' : copy.deepcopy(empty_array),
+        'digits' : [{'idx': [],'img': [],'classif': [],'note':[]} for i in range(nb_parts)]  
+    }   
 
     img = preprocess.read_image(args.input, 0)
     if preprocess.isGPformat(img):

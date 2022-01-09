710ebb8611df9ea50650757abdae189b0dff8ae0
Heloise Ouvry <heloiseouvry@gmail.com>
2022-01-09 19:49:43 +0100

    Updating the pipeline to apply on all parts

--- src/f1.py
+++ src/f1.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
 
     # img = preprocess.read_image(args.input, 0)
     img = preprocess.read_image(r'..\data\arpege.jpg', 0)
+    translated_img = copy.deepcopy(img)
     if preprocess.isGPformat(img):
         nb_parts = 5
         empty_array = [[] for i in range(nb_parts)]
@@ -29,36 +30,38 @@ if __name__ == "__main__":
             'staff_line' : copy.deepcopy(empty_array),
             'staff_col' : copy.deepcopy(empty_array),
             'digits' : [{'idx': [],'img': [],'classif': [],'note':[]} for i in range(nb_parts)]  
-        }  
+        }
         parts['original'] = preprocess.extract_parts(img)[0]
         parts['idx'] = preprocess.extract_parts(img)[1]
-        parts_0 = preprocess.invert_img(parts['original'][0])
-        thresh_0 = preprocess.thresh_img(parts_0)
+
+        for p in range(nb_parts):
+            parts['inv'][p] = preprocess.invert_img(parts['original'][p])
+            parts['thresh'][p] = preprocess.thresh_img(parts['inv'][p])
         
-        staff_idx = detect.staff_idx(parts_0)
-        col_idx = detect.col_idx(parts_0)
+            parts['staff_line'][p] = detect.staff_idx(parts['thresh'][p])
+            parts['staff_col'][p] = detect.col_idx(parts['thresh'][p])
 
-        removed = detect.remove_staff_idx(thresh_0, staff_idx)
-        removed = detect.remove_col_idx(removed, col_idx)
+            parts['wo_staff'][p] = detect.remove_staff_idx(parts['thresh'][p], parts['staff_line'][p])
+            parts['wo_staff'][p] = detect.remove_col_idx(parts['wo_staff'][p], parts['staff_col'][p])
 
-        dgt_idx = detect.get_digit_idx(removed)
-        dgt_img = detect.get_digit_img(removed)
+            parts['digits'][p]['idx'] = detect.get_digit_idx(parts['wo_staff'][p])
+            parts['digits'][p]['img'] = detect.get_digit_img(parts['wo_staff'][p])
 
-        # cv2.imwrite(r'..\results\parts_0.jpg',parts_0)
-        # cv2.imwrite(r'..\results\thresh_0.jpg',thresh_0)
-        # cv2.imwrite(r'..\results\removed.jpg',removed)
+            # cv2.imwrite(f'..\\results\\parts_{p}.jpg',parts['inv'][p])
+            # cv2.imwrite(f'..\\results\\thresh_{p}.jpg',parts['thresh'][p])
+            # cv2.imwrite(f'..\\results\\remove{p}.jpg',parts['wo_staff'][p])
 
-        translated_part = copy.deepcopy(parts['original'][0])
+            parts['translated'][p] = copy.deepcopy(parts['original'][p])
 
-        for (i,d) in enumerate(dgt_img):
-            dgt = classif.with_digit_template(d)
-            d_idx = dgt_idx[i]
-            note = classif.to_note(dgt,d_idx,staff_idx,notation='fr')
-            translated_part = postprocess.paste_note(translated_part,d_idx,note)
-        
-        translated_part = postprocess.bold_bottom_staff(translated_part, staff_idx)
-        # cv2.imwrite(r'..\results\translated_part.jpg',translated_part)
-        
-        # Pasting all back on the image
-        img[parts['idx'][0][0][0]:parts['idx'][0][1][0],parts['idx'][0][0][1]:parts['idx'][0][1][1]] = translated_part
-        cv2.imwrite(r'..\results\translated_img.jpg',img)
+            for (i,d) in enumerate(parts['digits'][p]['img']):
+                dgt = classif.with_digit_template(d)
+                d_idx = parts['digits'][p]['idx'][i]
+                note = classif.to_note(dgt,d_idx,parts['staff_line'][p],notation='fr')
+                parts['translated'][p] = postprocess.paste_note(parts['translated'][p],d_idx,note)
+            
+            parts['translated'][p] = postprocess.bold_bottom_staff(parts['translated'][p], parts['staff_line'][p])
+            # cv2.imwrite(f'..\\results\\parts_translated{p}.jpg',parts['translated'][p])
+            
+            # Pasting all back on the image
+            translated_img[parts['idx'][p][0][0]:parts['idx'][p][1][0],parts['idx'][p][0][1]:parts['idx'][p][1][1]] = parts['translated'][p]
+        cv2.imwrite(f'..\\results\\translated_img.jpg',translated_img)

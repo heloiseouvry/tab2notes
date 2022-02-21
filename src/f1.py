e06950cbd10c77a4ef09c70f0e6e1c1339ecd160
Heloise Ouvry <heloiseouvry@gmail.com>
2022-02-21 14:41:41 +0100

    Correction for slash in input_name
    Before only backslash was taken into account

--- src/f1.py
+++ src/f1.py
@@ -12,7 +12,10 @@ import cv2
 
 def translate(input, output, verbose=False):
     img = preprocess.read_image(input, 0)
-    input_name = input.split(chr(92))[-1].split(".")[0]
+    if chr(47) in input:
+        input_name = input.split(chr(47))[-1].split(".")[0]
+    if chr(92) in input:
+        input_name = input.split(chr(92))[-1].split(".")[0]
     # img = preprocess.read_image(r'..\data\arpege.jpg', 0)
     translated_img = copy.deepcopy(img)
     if preprocess.isGPformat(img):

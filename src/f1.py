7c0dcdc99d9019536b75a436d132a119c46de9f3
Heloise Ouvry <heloiseouvry@gmail.com>
2022-02-21 14:14:10 +0100

    translate function return translated_img

--- src/f1.py
+++ src/f1.py
@@ -72,8 +72,9 @@ def translate(input, output, verbose=False):
             # Pasting all back on the image
             translated_img[parts['idx'][p][0][0]:parts['idx'][p][1][0],parts['idx'][p][0][1]:parts['idx'][p][1][1]] = parts['translated'][p]
         if output:
-            cv2.imwrite(output + input_name + '_translated.jpg', translated_img)
-        return input_name + '_translated.jpg'
+            output_written = cv2.imwrite(output + input_name + '_translated.jpg', translated_img)
+            print(f'Output written: {output_written}')
+        return [translated_img, input_name + '_translated.jpg']
 
 # if __name__ == "__main__":
 #     translate(args.input, args.output, args.verbose)
